6a71dd7327287eeb224478ab487ee505e18b7afd
sanket chavan <sanketsh45@gmail.com>
2026-03-04 02:17:06 +0530

    Implemented structured logging using SLF4J and Logback with partition and offset tracing

--- src/main/java/com/webapp/orderKafka/consumer/AnalyticsConsumer.java
+++ src/main/java/com/webapp/orderKafka/consumer/AnalyticsConsumer.java
@@ -1,5 +1,9 @@
 package com.webapp.orderKafka.consumer;
 
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -7,9 +11,16 @@ import com.webapp.orderKafka.model.Order;
 
 @Component
 public class AnalyticsConsumer {
+	
+	public static final Logger logger = LoggerFactory.getLogger(AnalyticsConsumer.class);
 
 	@KafkaListener(topics = "orders-topic", groupId = "analytics-group")
-	public void analyze(Order order) {
-		System.out.println("Analytics Service -> " + order);
+	public void analyze(Order order, ConsumerRecord<String, Order> record) {
+		//System.out.println("Analytics Service -> " + order)
+		
+		logger.info("group=order-group partition={} offset={} orderId={}",
+		        record.partition(),
+		        record.offset(),
+		        order.getOrderId());
 	}
 }
--- src/main/java/com/webapp/orderKafka/consumer/OrderConsumer.java
+++ src/main/java/com/webapp/orderKafka/consumer/OrderConsumer.java
@@ -1,6 +1,8 @@
 package com.webapp.orderKafka.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +12,28 @@ import com.webapp.orderKafka.model.Order;
 public class OrderConsumer {
 
 
-	
+	private static final Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
 	
 	
 	@KafkaListener(topics = "orders-topic", groupId = "order-group")
 	public void consume(Order order, ConsumerRecord<String, Order> record) {
-		System.out.println("Consumer 1 | Partition: " 
-		        + record.partition() + " -> " + order);
+		
+//		System.out.println("Consumer 1 | Partition: " 
+//		        + record.partition() + " -> " + order);
+		logger.info("group=order-group partition={} offset={} orderId={}",
+		        record.partition(),
+		        record.offset(),
+		        order.getOrderId());
 	}
 	
 	@KafkaListener(topics = "orders-topic", groupId="order-group")
 	public void consume2(Order order, ConsumerRecord<String, Order> record) {
-		System.out.println("Consumer 2 | Partition: " 
-		        + record.partition() + " -> " + order);
+//		System.out.println("Consumer 2 | Partition: " 
+//		        + record.partition() + " -> " + order);
+		logger.info("group=order-group partition={} offset={} orderId={}",
+		        record.partition(),
+		        record.offset(),
+		        order.getOrderId());
 	}
 	
 	
